6d5b403ce9107b719d75f0da622bbbc6712d2a04
Vahid Mostofi <vahid@Vahids-MacBook-Pro.local>
2020-04-18 10:01:13 -0600

    version2

--- core.go
+++ core.go
@@ -1,7 +1,13 @@
 package main
 
+// TracesInfo ...
+type TracesInfo struct {
+	Info  map[string][]map[string]interface{} `json:"info"`
+	Stats map[string]map[string]*Stats        `json:"stats"`
+}
+
 // GatherTraceInfo ...
-func GatherTraceInfo(f *FetcherInput, traceAggregator TraceAggregator) (map[string][]map[string]interface{}, error) {
+func GatherTraceInfo(f *FetcherInput, traceAggregator TraceAggregator) (*TracesInfo, error) {
 	traceFetcher := &SimpleTraceFetcher{}
 	traces, err := traceFetcher.FetchTraces(f)
 	if err != nil {
@@ -21,5 +27,11 @@ func GatherTraceInfo(f *FetcherInput, traceAggregator TraceAggregator) (map[stri
 		detailsMap[trace.TraceType] = append(detailsMap[trace.TraceType], details)
 	}
 
-	return detailsMap, nil
+	t := &TracesInfo{}
+	t.Info = detailsMap
+	t.Stats = make(map[string]map[string]*Stats)
+	for key, traceInfo := range t.Info {
+		t.Stats[key] = ComputeStats(traceInfo)
+	}
+	return t, nil
 }
--- main.go
+++ main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	// f := &FetcherInput{
-	// 	URL:         "http://server204:16686",
-	// 	End:         0,
-	// 	Start:       1587099027978000,
-	// 	Limit:       10,
-	// 	Lookback:    "",
-	// 	MaxDuration: 0,
-	// 	MinDuration: 0,
-	// 	Service:     "gateway",
-	// }
-
-	// res, err := GatherTraceInfo(f, &FixedBookstoreTraceParser{})
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	controller := &Controller{}
-	http.ListenAndServe(":9890", controller)
-
+	port := flag.String("port number", "8657", "port to listent to")
+	fmt.Println("server started and listening to port", *port)
+	c := &Controller{}
+	http.ListenAndServe(":"+*port, c)
 }
